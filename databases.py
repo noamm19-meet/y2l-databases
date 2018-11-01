493492652292cabae201c49da3ef2f0748fad009
aboud20-meet <aboud20@meet.mit.edu>
2018-11-01 18:44:12 +0200

    data

--- databases.py
+++ databases.py
@@ -9,17 +9,38 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # Write your functions to interact with the database here :
+def add_product(name , year , tipe , price):
+ 	product_object = Product(
+ 		name = name,
+ 		year = year,
+ 		tipe = tipe,
+ 		price = price)
+ 	session.add(product_object)
+ 	session.commit()
+ 	
 
 def create_product():
   #TODO: complete the functions (you will need to change the function's inputs)
+
   pass
 
-def update_product():
+def update_product(tipe , year , price):
   #TODO: complete the functions (you will need to change the function's inputs)
-  pass
+  product_object = session.query(
+  	Product).filter_by(tipe = tipe).all()
+  product_object.year = year
+  product_object.price = price
+  session.commit()
+ 
 
 def delete_product(id):
-  pass
+	session.query(product).filter_by(
+		id=id).delete()
+	session.commit()
+  
 
 def get_product(id):
-  pass
+	product = session.query(
+		Product).filter_by(id = id)
+	return product
+  
--- model.py
+++ model.py
@@ -8,4 +8,20 @@ Base = declarative_base()
 # Write your classes here :
 class Product(Base):
     # TODO: complete this class
-    pass
+    __tablename__ = "Product"
+    Product_id = Column(Integer , Priamary_key = True)
+    name = Column(String)
+    year = Column(Integer)
+    tipe = Column(String)
+    price = Column(Integer)
+
+ def __repr__(self):
+ 	return ("Product name: {} \n"
+ 		"Product expiration date: {} \n"
+ 		"Product tipe: {}\n"
+ 		"Product price: {}").format(
+ 		self.name,
+ 		self.year,
+ 		self.tipe,
+ 		self.price)
+ 	
